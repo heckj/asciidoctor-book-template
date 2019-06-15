f0f45e3264140e3293a2a04a66a3d6cedb48489f
Joe Heck <heckj@mac.com>
2019-06-15 15:23:46 -0700

    sample book setup

--- asciidoc_install.sh
+++ asciidoc_install.sh
@@ -1,6 +1,6 @@
-# Asciidoctor Book Template
-
-## Setup (Mac)
+# rbenv version installation
+rbenv install 2.6.3
+rbenv global 2.6.3
 
 gem install asciidoctor
 NOKOGIRI_USE_SYSTEM_LIBRARIES=1 gem install asciidoctor-epub3 --pre
